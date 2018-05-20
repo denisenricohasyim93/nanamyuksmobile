e706b1ad903c023dcb835ddd32e9b0c220fa42ef
denisenricohasyim93 <denisenricohasyim93@gmail.com>
2018-05-20 15:32:02 +0700

    another commit

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -26,38 +26,38 @@ export default class Menu extends Component<{}> {
                 <Image  style={{width:96, height: 96}}
                   source={require('../images/icons8-plant-96.png')}/>
               </TouchableOpacity>
-              <Text style={{fontSize: 15, color: 'white', fontWeight: 'bold'}}>My Plants</Text>
+              <Text style={{fontSize: 15, color: '#f1c40f', fontWeight: 'bold'}}>My Plants</Text>
             </View>
             <View style={{flex: 1, flexDirection: 'column',alignItems: 'center', justifyContent: 'center',backgroundColor: '#92B500', width: width/2, height: height/3}}>
               <TouchableOpacity onPress={() => Actions.plantationstutorial()}>
                 <Image  style={{width:96, height: 96}}
                   source={require('../images/icons8-classroom-96.png')}/>
               </TouchableOpacity>
-              <Text style={{fontSize: 15, color: 'white', fontWeight: 'bold'}}>Plantations Tutorial</Text>
+              <Text style={{fontSize: 15, color: '#035023', fontWeight: 'bold'}}>Plantations Tutorial</Text>
             </View>
         </View>
         <View style={{flexDirection: 'row'}}>
           <View style={{flex: 1, flexDirection: 'column',alignItems: 'center', justifyContent: 'center',backgroundColor: '#92B500', width: width/2, height: height/3}}>
             <Image  style={{width:96, height: 96}}
               source={require('../images/icons8-share-96.png')}/>
-            <Text style={{fontSize: 15, color: 'white', fontWeight: 'bold'}}>Communities Sharing</Text>
+            <Text style={{fontSize: 15, color: '#035023', fontWeight: 'bold'}}>Communities Sharing</Text>
           </View>
           <View style={{flex: 1, flexDirection: 'column',alignItems: 'center', justifyContent: 'center',backgroundColor: '#77533B', width: width/2, height: height/3}}>
             <Image  style={{width:96, height: 96}}
               source={require('../images/icons8-small-business-96.png')}/>
-            <Text style={{fontSize: 15, color: 'white', fontWeight: 'bold'}}>Products Storefront</Text>
+            <Text style={{fontSize: 15, color: '#f1c40f', fontWeight: 'bold'}}>Products Storefront</Text>
           </View>
         </View>
         <View style={{flexDirection: 'row'}}>
           <View style={{flex: 1, flexDirection: 'column',alignItems: 'center', justifyContent: 'center',backgroundColor: '#77533B', width: width/2, height: height/3}}>
             <Image  style={{width:96, height: 96}}
-              source={require('../images/icons8-caliper-96.png')}/>
-            <Text style={{fontSize: 15, color: 'white', fontWeight: 'bold'}}>Tools Marketplace</Text>
+              source={require('../images/icons8-hammer-96.png')}/>
+            <Text style={{fontSize: 15, color: '#f1c40f', fontWeight: 'bold'}}>Tools Marketplace</Text>
           </View>
           <View style={{flex: 1, flexDirection: 'column',alignItems: 'center', justifyContent: 'center',backgroundColor: '#92B500', width: width/2, height: height/3}}>
             <Image  style={{width:96, height: 96}}
               source={require('../images/icons8-male-user-96.png')}/>
-            <Text style={{fontSize: 15, color: 'white', fontWeight: 'bold'}}>My Profile</Text>
+            <Text style={{fontSize: 15, color: '#035023', fontWeight: 'bold'}}>My Profile</Text>
           </View>
         </View>
       </View>
--- src/pages/MyPlants.js
+++ src/pages/MyPlants.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  Text,
+  // Text,
   View,
   StatusBar ,
   TouchableOpacity,
   Dimensions,
-  Image
+  Image,
+  ScrollView
 } from 'react-native';
+import { 
+  Container, 
+  Header, 
+  Content, 
+  Card, 
+  CardItem, 
+  Text, 
+  Icon, 
+  Right,
+  Body,
+  Left
+} from 'native-base';
 
 import Logo from '../components/Logo';
 import Form from '../components/Form';
@@ -15,12 +28,73 @@ import Form from '../components/Form';
 import {Actions} from 'react-native-router-flux';
 var { height, width } = Dimensions.get("window");
 
+import Gambar from 'react-native-image-progress';
+import ProgressBar from 'react-native-progress/Bar';
+
 export default class MyPlants extends Component<{}> {
 	render() {
+    const TANAMAN = [
+      {
+        "Gambar" : "https://www.growweedeasy.com/wp-content/uploads/2013/05/purple-side-bud-cola.jpg",
+        "PotNumber" : "1",
+        "SoilSensorValue" : "NORMAL",
+        "Temperature" : "NORMAL",
+        "STATE" : "IS IN NORMAL STATE",
+      },
+      {
+        "Gambar" : "https://www.growweedeasy.com/wp-content/uploads/2012/04/dinafem-blue-thai-cannabis-plant-grown-in-air-pot.jpg",
+        "PotNumber" : "2",
+        "SoilSensorValue" : "DRY",
+        "Temperature" : "NORMAL",
+        "STATE" : "NEEDS WATER",
+      },
+      {
+        "Gambar" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_vlbhwk2QI6b_RhWou_gqimwHknnqJ4tS1tvrOON7MZYfDUsk",
+        "PotNumber" : "3",
+        "SoilSensorValue" : "WET",
+        "Temperature" : "NORMAL",
+        "STATE" : "NEEDS SUN",
+      },
+    ];
 		return(
-            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white'}}>
-                <Text>My Plants</Text>
-            </View>    
+      <ScrollView>
+        <View>
+          {TANAMAN.map((innerArray, index) => {
+            return (
+              <Card style={{height: null}}>
+                <CardItem>
+                  <Body style={{alignItems: 'flex-end', justifyContent: 'center'}}>
+                    <Text>#PotNumber {innerArray.PotNumber}</Text>
+                  </Body>
+                </CardItem>
+                <CardItem>
+                  <Body style={{alignItems: 'center', justifyContent: 'center'}}>
+                    <Gambar 
+                      source={{uri: innerArray.Gambar}} 
+                      style={{width: 0.9*width, height: 0.9*width, borderColor: "black", borderWidth: 5}}
+                    />
+                  </Body>
+                </CardItem>
+                <CardItem>
+                  <Body>
+                    <Text>Soil Sensor Value : {innerArray.SoilSensorValue}</Text>
+                  </Body>
+                </CardItem>
+                <CardItem>
+                  <Body>
+                    <Text>Temperature : {innerArray.Temperature}</Text>
+                  </Body>
+                </CardItem>
+                <CardItem>
+                  <Body>
+                    <Text>The Plant {innerArray.STATE}</Text>
+                  </Body>
+                </CardItem>
+              </Card>
+            )
+          })}
+        </View>
+      </ScrollView>   
 		)
 	}
 }
